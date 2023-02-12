c0ff97d28417c45bbc38afac8e31b688f412dc90
DivyankR <divyankrawat2021@gmail.com>
2023-02-12 17:06:07 +0530

    updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HYPHEN_E_DOT = "-e ."
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_file]
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
 
 setup(
     name="sensor",
