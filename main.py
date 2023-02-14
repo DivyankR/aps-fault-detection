2a636b34aecec0ad1adff756663eb4630d73c13b
DivyankR <divyankrawat2021@gmail.com>
2023-02-14 08:33:43 +0530

    updated

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import sys,os
 
 if __name__ == "__main__":
      try:
-          pass
+          get_collection_as_dataframe(database_name="aps",collection_name="sensor")
      except Exception as e:
           print(e)
