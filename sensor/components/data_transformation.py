d097fb3e11a294a5cbafb0798b6d12e5fab3876e
DivyankR <divyankrawat2021@gmail.com>
2023-02-21 11:03:33 +0530

    updated

--- sensor/components/data_transformation.py
+++ sensor/components/data_transformation.py
@@ -27,6 +27,7 @@ class DataTransformation:
         except Exception as e:
             raise SensorException(e, sys)
 
+
     @classmethod
     def get_data_transformer_object(cls)->Pipeline:
         try:
@@ -39,6 +40,7 @@ class DataTransformation:
             return pipeline
         except Exception as e:
             raise SensorException(e, sys)
+
 
     def initiate_data_transformation(self,) -> artifact_entity.DataTransformationArtifact:
         try:
@@ -69,7 +71,7 @@ class DataTransformation:
             input_feature_test_arr = transformation_pipleine.transform(input_feature_test_df)
             
 
-            smt = SMOTETomek(sampling_strategy="minority")
+            smt = SMOTETomek(random_state=42)
             logging.info(f"Before resampling in training set Input: {input_feature_train_arr.shape} Target:{target_feature_train_arr.shape}")
             input_feature_train_arr, target_feature_train_arr = smt.fit_resample(input_feature_train_arr, target_feature_train_arr)
             logging.info(f"After resampling in training set Input: {input_feature_train_arr.shape} Target:{target_feature_train_arr.shape}")
@@ -111,4 +113,5 @@ class DataTransformation:
             return data_transformation_artifact
         except Exception as e:
             raise SensorException(e, sys)
+
 
